8dcb6091528381a0e001398deaeb316f534a33c3
vradu <vradu@EN30309.endava.net>
2012-11-12 19:59:29 +0300

    removed unused password2 field from User.java

--- src/main/java/org/pinboard/web/Credentials.java
+++ src/main/java/org/pinboard/web/Credentials.java
@@ -7,29 +7,25 @@ import javax.inject.Named;
 @RequestScoped
 public class Credentials {
 
-    
+	private String username;
+	private String password;
+	private String password2;
 
-    private String username;
+	public String getUsername() {
+		return username;
+	}
 
-    private String password;
-    
-    private String password2; 
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
-    
+	public String getPassword() {
+		return password;
+	}
 
-    public String getUsername() {
-    	return username; }
-
-    public void setUsername(String username) {
-    	this.username = username; }
-
-    
-
-    public String getPassword() {
-    	return password; }
-
-    public void setPassword(String password) {
-    	this.password = password; }
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 	public String getPassword2() {
 		return password2;
@@ -38,7 +34,4 @@ public class Credentials {
 	public void setPassword2(String password2) {
 		this.password2 = password2;
 	}
-
-    
-
 }
--- src/main/java/org/pinboard/web/User.java
+++ src/main/java/org/pinboard/web/User.java
@@ -10,7 +10,6 @@ public class User {
 	@Id
 	private String username;
 	private String password;
-	private String password2;
 	
 	public User() {
 	}
@@ -31,14 +30,6 @@ public class User {
 	}
 	public void setPassword(String password) {
 		this.password = password;
-	}
-
-	public String getPassword2() {
-		return password2;
-	}
-
-	public void setPassword2(String password2) {
-		this.password2 = password2;
 	}
 	
 	/*@Override
